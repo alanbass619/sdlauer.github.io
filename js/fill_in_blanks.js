bd192dbc4b3d9c8affe16b484678671d67314d73
sdlauer <sdlauer@gmail.com>
2022-05-26 17:57:12 -0400

    update FITB

--- js/fill_in_blanks.js
+++ js/fill_in_blanks.js
@@ -1,47 +1,31 @@
-$(document).ready(function() {
-    // This code used for set order attribute for items
-  var numberOfItems = $("#optionsFiB").find('li').length;
-  $.each($("#optionsFiB").find('li'), function(index, item) {
-      $(item).attr("order", index);
-      var removeButton = $('<i class="fa fa-times" style="display:none"></i>');
-      removeButton.click(function(){
-           addToOlderPlace($(this).parent());
-        });
-      $(item).append(removeButton);
-  });
-    $("span").droppable({
-      accept: "li",
-      classes: {
-        "ui-droppable-hover": "ui-state-hover"
-      },
-      drop: function(event, ui) {
-      // Check for existing another option
-      if($(this).find('li').length > 0)
-      addToOlderPlace($(this).find('li'));
-        $(this).addClass("ui-state-highlight");
-        $(this).addClass('matched');
-        $(ui.draggable).find('i').attr("style","");
-        $(this).append($(ui.draggable));
-      }
+$(function () {
+    $("#optionsFiB,span").sortable({
+        connectWith: "#optionsFiB,span",
+        start: function (event, ui) {
+            ui.item.toggleClass("highlight");
+        },
+        receive: function(event, ui) {
+            var list = $(this);
+            if (list.attr('id') != "optionsFiB") {
+                if (list.children().length > 1) {                
+                    var tspanElem = list.find(":first-child").detach();
+                    // tspanElem.removeAttr("style");
+                    $("#optionsFiB").append(tspanElem);
+                    ui.css("color","purple");
+                }
+            }
+            $this.css("color","purple");
+        },
+        stop: function (event, ui) {
+            // ui.item.toggleClass("highlight");
+            // https://www.geeksforgeeks.org/jquery-ui-sortable-refreshpositions-method/
+            var children = $(this)
+                .sortable('refreshPositions')
+                .children();
+            // liHt();
+        },
     });
-    // $("li").sortable();
-    $("li").draggable({
-       helper:"clone",
-      revert: "invalid"
-    });
-    // This function used for find old place of item
-    function addToOlderPlace($item) {
-          var indexItem = $item.attr('order');
-          var itemList = $("#optionsFiB").find('li');
-          $item.find('i').hide();
-          if (indexItem === "0")
-              $("#optionsFiB").prepend($item);
-          else if (Number(indexItem) === (Number(numberOfItems)-1))
-                $("#optionsFiB").append($item);
-          else
-              $(itemList[indexItem - 1]).after($item);
-      }
-  });
+});
 
   function shuffle(items) {
     var cached = items.slice(0), temp, i = cached.length, rand;
