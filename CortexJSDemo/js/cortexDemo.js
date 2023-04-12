244bfcdfc7d3d344b32a104c6123e3dcf9f843cb
sdlauer <sdlauer@gmail.com>
2023-04-12 12:30:04 -0400

    update

--- CortexJSDemo/js/cortexDemo.js
+++ CortexJSDemo/js/cortexDemo.js
@@ -46,49 +46,62 @@ function approxCE(x, num, sieve) {
     let out1 = "Incorrect";
     let exprUn = "doesn't work";
     let exprAn = "doesn't work";
-    try {
-    exprUn = ce.parse(mathFieldUser.value).N();
-    exprAn = ce.parse(textAreaAuthor.value).N();
-    }
-    catch (err){}
+    try { exprUn = ce.parse(mathFieldUser.value).N(); }
+    catch (err) { exprUn = 'LaTeX'; }
+    try { exprAn = ce.parse(textAreaAuthor.value).N(); }
+    catch (err) { exprAn = 'LaTeX'; }
     let exprUeval = ce.parse(mathFieldUser.value).evaluate();
     let exprAeval = ce.parse(textAreaAuthor.value).evaluate();
-    let outVars = "USER VALUES:\n  document.querySelector('#latexUserAns' + num)\n    =" + mathFieldUser + "\n    mathFieldUser.value \n     =" + mathFieldUser.value 
-                    + "\n    ce.parse(mathFieldUser.value).simplify().latex;\n     =" + exprUsimp +"\n    ce.parse(mathFieldUser.value).evaluate()\n     =" + exprUeval 
-                    +"\n    ce.parse(mathFieldUser.value).N()\n     =" + exprUn
-                    + "\n\nAUTHOR VALUES\n    document.querySelector('#MyauthorAns' + num)\n    =" + textAreaAuthor + "\n    textAreaAuthor.value \n     =" + textAreaAuthor.value 
-                    + "\n    ce.parse(textAreaAuthor.value).simplify().latex \n     =" + exprAsimp +"\n    ce.parse(textAreaAuthor.value).evaluate() \n     =" + exprAeval 
-                    +"\n    ce.parse(textAreaAuthor.value).N() \n     =" + exprAn;
+    let outVars = "USER VALUES:\n  document.querySelector('#latexUserAns' + num)\n    =" + mathFieldUser + "\n    mathFieldUser.value \n     =" + mathFieldUser.value
+        + "\n    ce.parse(mathFieldUser.value).simplify().latex;\n     =" + exprUsimp + "\n    ce.parse(mathFieldUser.value).evaluate()\n     =" + exprUeval
+        + "\n    ce.parse(mathFieldUser.value).N()\n     =" + exprUn
+        + "\n\nAUTHOR VALUES\n    document.querySelector('#MyauthorAns' + num)\n    =" + textAreaAuthor + "\n    textAreaAuthor.value \n     =" + textAreaAuthor.value
+        + "\n    ce.parse(textAreaAuthor.value).simplify().latex \n     =" + exprAsimp + "\n    ce.parse(textAreaAuthor.value).evaluate() \n     =" + exprAeval
+        + "\n    ce.parse(textAreaAuthor.value).N() \n     =" + exprAn;
     let out2 = textAreaAuthor.value + " \\approx " + exprAeval;
     if (exprUsimp == "" || (exprUeval == '["Sequence"]')) out1 = "Incorrect\nNO USER INPUT";
     else {
         switch (sieve) {
+            // Want a LaTeX expression for the answer
             case 'latex':
-                if (mathFieldUser.value == textAreaAuthor.value)
+                // The expected answer
+                if (mathFieldUser.value == textAreaAuthor.value && exprUn == exprAn)
                     out1 = "Correct  \nLaTeX (user <==> author) = \n    " + mathFieldUser.value + "\n==\n    " + textAreaAuthor.value;
-
-                else if (exprUsimp == exprAsimp)
-                     out1 = "Correct, but different order";
-                    else if (exprUeval == exprAsimp) out1 += "\nThe answer of " + mathFieldUser.value + " is a correct approximation, but an expression is expected, not a numerical value.\nLaTeX (user NOT IDENTICAL author) = " + mathFieldUser.value + " IS NOT IDENTICAL TO\n    " + textAreaAuthor.value;
-
-                else if (mathFieldUser.value == exprUeval) out1 += "\nAn expression is expected.";
+                // Answer in a different order
+                else if (exprUsimp == exprAsimp && exprUn == exprAn)
+                    out1 = "Correct, but different order\nExpected " + textAreaAuthor.value;
+                // Numerical answer, but needed an expression
+                else if (exprUeval == exprAsimp) out1 += "\nThe answer " + mathFieldUser.value + " is a correct approximation, but an expression is expected, not a numerical value.\n\n" + mathFieldUser.value + " IS NOT IDENTICAL TO " + textAreaAuthor.value;
+                // Neither a correct number nor expression
                 else out1 += "\nThe expression contains an error.";
                 break;
             case 'approx':
-                out1 += "\nA numerical approximation is expected.";
+                if (exprUeval == exprAsimp){
+                    out1 = "Correct. ";
+                    if (mathFieldUser.value == exprUsimp)
+                       out1 += "The expected answer is " + textAreaAuthor.value; 
+                    else 
+                        out1 += "However, a number is expected, not an expression.";
+                    break;
+                }
+                
             case 'exact':
                 let exprUvalOf = ce.parse(mathFieldUser.value).N().valueOf();
                 let exprAvalOf = ce.parse(textAreaAuthor.value).N().valueOf();
                 out2 = exprAsimp + " \\approxeq " + exprAvalOf.toFixed(p);
-                outVars += "\n\nIf number:\nUSER    ce.parse(mathFieldUser.value).N().valueOf()\n     = " + exprUvalOf + "\nAUTHOR    ce.parse(textAreaAuthor.value).N().valueOf() \n    =" + exprAvalOf + "\n    exprAvalOf.toFixed(p)\n    =" + exprAvalOf.toFixed(p);  
-                if (exprUsimp == exprAsimp) out1 = "Incorrect\nA simple numerical answer is expected, not an expression.\n" +
-                    "User input: " + mathFieldUser.value + "    Expected input: " + exprAvalOf.toFixed(p);
+                outVars += "\n\nIf number:\nUSER    ce.parse(mathFieldUser.value).N().valueOf()\n     = " + exprUvalOf +
+                    "\nAUTHOR    ce.parse(textAreaAuthor.value).N().valueOf() \n    =" + exprAvalOf + "\n    exprAvalOf.toFixed(p)\n    =" + exprAvalOf.toFixed(p);
+                if (exprUsimp == exprAsimp && exprUn != exprAn) 
+                    out1 = "Correct. However, A simple numerical answer is expected, not an expression.\n" +
+                        "User input: " + mathFieldUser.value + "    Expected input: " + exprAvalOf.toFixed(p);
                 else {
 
                     if ((exprAvalOf - prec) < exprUvalOf && exprUvalOf < (exprAvalOf + prec)) {
-                        out1 = "Correct" + "\nFor: x = " + x + "\nLaTeX = " + exprUsimp + " ? " + exprAsimp + "\napproximation (user <--> author) = " + exprUvalOf + " <--> " + exprAvalOf;;
+                        out1 = "Correct" + "\nFor: x = " + x + "\nLaTeX = " + exprUsimp + " ? " + exprAsimp + 
+                            "\napproximation (user <--> author) = " + exprUvalOf + " <--> " + exprAvalOf;;
 
                     }
+                    else out1 = "Incorrect value.  Expected answer is " + exprAsimp;
                 }
                 break;
             default:
